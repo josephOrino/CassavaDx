4b99535beb695b6ac5cda700400b60e2fd23e1cb
Mark Joseph B. Orino <orino.mj22@gmail.com>
2024-09-21 12:20:27 +0800

    Added pop up in camera button

--- app/src/main/java/com/example/cassavadx/MainActivity.java
+++ app/src/main/java/com/example/cassavadx/MainActivity.java
@@ -1,6 +1,20 @@
 package com.example.cassavadx;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         replaceFragment(new HomeFragment());
         binding.bottomNavigationView.setBackground(null);
 
+        // Opening navigation menus
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.home) {
@@ -48,12 +63,80 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+
+        // Opening camera button on navigation bar
+        fab = findViewById(R.id.fab);
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomDialog();
+            }
+        });
+
     }
+
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
 
+    }
+
+    private void showBottomDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_sheet_layout);
+
+        LinearLayout cameraLayout = dialog.findViewById(R.id.layoutCamera);
+        LinearLayout galleryLayout = dialog.findViewById(R.id.layoutGallery);
+        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
+
+        cameraLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if(checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+//                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    //startActivityForResult(cameraIntent, 3);
+//                    cameraLauncher.launch(cameraIntent);
+//                } else {
+//                    requestPermissions(new String[]{Manifest.permission.CAMERA},100);
+//                }
+
+//                replaceFragment(new PredictionFragment());
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Open camera is clicked",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        galleryLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the gallery to pick an image
+//                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                galleryLauncher.launch(galleryIntent);
+//
+//                replaceFragment(new PredictionFragment());
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Choose from gallery is clicked",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }
